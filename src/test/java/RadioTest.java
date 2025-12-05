58a2ccf9a8a75abed773a1b11001d21e1e7193d5
Leonid <followers@mail.ru>
2025-12-05 18:07:59 +0500

    Complete tests

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -14,7 +14,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     @Test
@@ -26,7 +25,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     @Test
@@ -38,7 +36,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
 
@@ -53,7 +50,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     @Test
@@ -65,7 +61,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     @Test
@@ -77,7 +72,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     // Граничные значения радиостанций: 8 , 9 , 10
@@ -91,7 +85,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     @Test
@@ -103,7 +96,6 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
     @Test
@@ -115,11 +107,10 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
 
-    // Тестирование переключения следующей и предыдущай радиостанции
+    // Тестирование переключения следующей и предыдущей радиостанции
 
     @Test
     public void nextStation() {
@@ -131,9 +122,19 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
+    @Test
+    public void nextStationFromNine() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.next();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
     public void previousStation() {
@@ -145,13 +146,23 @@ public class RadioTest {
         int actual = radio.getCurrentStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
 
+    @Test
+    public void previousStationFromZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.prev();
 
-    // Граничные значения 1 , 0 , -1
-    // Увеличение громкости на один пункт
+        int expected = 9;
+        int actual = radio.getCurrentStation();
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Тестирование громкости
+
+    @Test
     public void shouldIncreaseVolume() {
         Radio radio = new Radio();
         radio.increaseVolume();
@@ -162,10 +173,10 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    // Уменьшение громкости на один пункт
-
+    @Test
     public void shouldDecreaseVolume() {
         Radio radio = new Radio();
+        radio.increaseVolume(); // Сначала увеличиваем до 1
         radio.decreaseVolume();
 
         int expected = 0;
@@ -174,13 +185,10 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-
-    // Уменьшение громкости на один пункт для проверки граничного значения.
-    // Должно получиться 0 , а не -1
-
-    public void shouldDecreaseVolume2() {
+    @Test
+    public void shouldNotDecreaseVolumeBelowZero() {
         Radio radio = new Radio();
-        radio.decreaseVolume();
+        radio.decreaseVolume(); // Пытаемся уменьшить с 0
 
         int expected = 0;
         int actual = radio.getCurrentVolume();
@@ -188,11 +196,7 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-
-
-    // Увеличение громкости на один пункт 99 раз
-    // для проверки граничных значений 99, 100, 101
-
+    @Test
     public void shouldIncreaseVolumeTo99() {
         Radio radio = new Radio();
 
@@ -206,10 +210,13 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-
+    @Test
     public void shouldIncreaseVolumeTo100() {
         Radio radio = new Radio();
-        radio.increaseVolume();
+
+        for (int i = 0; i < 100; i++) {
+            radio.increaseVolume();
+        }
 
         int expected = 100;
         int actual = radio.getCurrentVolume();
@@ -217,8 +224,16 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    public void tryingIncreaseVolumeTo101() {
+    @Test
+    public void shouldNotIncreaseVolumeAbove100() {
         Radio radio = new Radio();
+
+        // Увеличиваем до 100
+        for (int i = 0; i < 100; i++) {
+            radio.increaseVolume();
+        }
+
+        // Пытаемся увеличить сверх лимита
         radio.increaseVolume();
 
         int expected = 100;
@@ -227,13 +242,35 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldDecreaseVolumeFrom100() {
+        Radio radio = new Radio();
 
+        // Увеличиваем до 100
+        for (int i = 0; i < 100; i++) {
+            radio.increaseVolume();
+        }
 
+        // Уменьшаем
+        radio.decreaseVolume();
 
+        int expected = 99;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkDefaultValues() {
+        Radio radio = new Radio();
+
+        int expectedStation = 0;
+        int actualStation = radio.getCurrentStation();
+
+        int expectedVolume = 0;
+        int actualVolume = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expectedStation, actualStation);
+        Assertions.assertEquals(expectedVolume, actualVolume);
+    }
 }
-
-
-
-
-
-
